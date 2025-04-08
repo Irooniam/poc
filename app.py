4134746091384818dfd71b88880fa39bbc709702
bob <irooniam@gmail.com>
2025-04-08 10:33:22 -0700

    initial setup

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from dotenv import load_dotenv
+from modules import models 
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+if not load_dotenv():
+    exit("do not have .env file")
+
+app = Flask(__name__)
+
+
+#db config setup
+connStr = "postgresql://{u}:{p}@{h}/{d}".format(
+        u=os.environ["DB_USER"], 
+        p=os.environ["DB_PASSWORD"], 
+        h=os.environ["DB_HOST"], 
+        d=os.environ["DB_NAME"]
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = connStr 
+models.db.init_app(app)
+
+@app.route("/")
+def hello_world():
+    return "Glorious index page"
+
+@app.route("/lead", methods=["POST"])
+def add_lead():
+    return "a new lead"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ modules/models.py
@@ -0,0 +1,25 @@
+import psycopg2
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    __tablename__ = "leads"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
+    address1: Mapped[str] = mapped_column(String(100))
+    address2: Mapped[str] = mapped_column(String(100))
+    city: Mapped[str] = mapped_column(String(100))
+    state: Mapped[str] = mapped_column(String(2))
+    city: Mapped[str] = mapped_column(String(100))
+    zip: Mapped[int] = mapped_column(Integer)
+    phone: Mapped[str] = mapped_column(String(20))
+
